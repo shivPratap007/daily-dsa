905994ee9ef759a4e06e65db94954f6db3d012bd
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-04-03 03:30:00 +0530

    lcm and hcf

---
+++ basic/lcmandhcf.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int findHCF(int n1, int n2){
+    int n=n2;
+    while(n!=0){
+        if(n1%n==0 && n2%n==0){
+            return n;
+        }
+        n--;
+    }
+}
+
+int main(){
+    int n1;
+    int n2;
+    cout << "Enter first no." << endl;
+    cin >> n1;
+    cout << "Enter second no." << endl;
+    cin >> n2;
+    
+    if(n1<n2){
+        int temp=n1;
+        n1=n2;
+        n2=temp;
+    }
+    
+    int hcf=findHCF(n1,n2);
+    cout << hcf << endl;
+    int lcm=(n1*n2)/hcf;
+    cout << "LCM IS: " << lcm << endl;
+    // lcm=(n1*n2)/hcf
+    
+    
+}
