1b2ac4addec4bf5f1f092625f57acf92a380460f
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-04-03 03:41:13 +0530

    optimized approach for lcm and hcf

--- basic/lcmandhcf.cpp
+++ basic/lcmandhcf.cpp
@@ -33,3 +33,34 @@ int main(){
     
     
 }
+// #include<iostream>
+// using namespace std;
+
+// int findHCF(int n1, int n2){
+    
+//     while(n1%n2!=0){
+//         int rem=n1%n2;
+//         n2=n1;
+//         n1=rem;
+        
+//     }
+//     return n1;
+// }
+
+// int main(){
+//     int n1;
+//     int n2;
+//     cout << "Enter first no." << endl;
+//     cin >> n1;
+//     cout << "Enter second no." << endl;
+//     cin >> n2;
+    
+    
+    
+//     int hcf=findHCF(n1,n2);
+//     cout << hcf << endl;
+//     int lcm=(n1*n2)/hcf;
+//     cout << "LCM IS: " << lcm << endl;
+    
+    
+// }
