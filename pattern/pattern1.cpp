2c4d1d0bf5d13c6f8ef672ab1a2ef6f08fa013d0
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-04-03 22:07:40 +0530

    pattern1

---
+++ pattern/pattern1.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+void printPattern(int n){
+    for(int i=0; i<n ; i++){
+        for(int j=0; j<n; j++){
+            if(j<=i){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter the input " << endl;
+    cin >> n;
+    printPattern(n);
+}
+
+#include<iostream>
+using namespace std;
+
+void printPattern(int n){
+    for(int i=0; i<n ; i++){
+        for(int j=0; j<n; j++){
+            if(j<=i){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter the input " << endl;
+    cin >> n;
+    printPattern(n);
+}
+
+#include<iostream>
+using namespace std;
+
+void printPattern(int n){
+    for(int i=0; i<n ; i++){
+        for(int j=0; j<n; j++){
+            if(j<=i){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter the input " << endl;
+    cin >> n;
+    printPattern(n);
+}
+
+// 5
+// *    
+// **   
+// ***  
+// **** 
+// *****
