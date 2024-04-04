a7999e6f09f24c3d49f5cb69e9f7d524e03da716
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-04-04 21:21:29 +0530

    patern7

---
+++ pattern/pattern7.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+
+using namespace std;
+void PrintPattern(int n){
+    int sp1=n/2;
+    int sp2=0;
+    int st=1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=sp1; j++){
+            cout << "-";
+        }
+        cout << "*";
+        for(int l=1; l<=sp2; l++ ){
+            cout << "-";
+        }
+        if(i>1 && i<n){
+            cout << "*";
+        }
+        
+        cout << endl;
+        if(i<(n-2)){
+            sp1--;
+        }else{
+            sp1++;
+        }
+        if(i==1 || i==n){
+            sp2=1;
+        }else if(i<(n-2)){
+            sp2+=2;
+        }else{
+            sp2-=2;
+        }
+        
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter the no.";
+    cin >> n;
+    PrintPattern(n);
+}
+
+// Only works for n=5 till now
+// Enter the no.5
+// --*
+// -*-*
+// *---*
+// -*-*
+// --*
